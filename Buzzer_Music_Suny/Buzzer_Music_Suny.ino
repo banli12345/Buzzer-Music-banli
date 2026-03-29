79283d0e9b2ba87b4b7d586f399eecc096e6a27d
banli12345 <someoneisbanli@outlook.com>
2026-03-29 13:23:28 +0800

    Add files via upload

---
+++ Buzzer_Music_Suny/Buzzer_Music_Suny.ino
@@ -0,0 +1,13 @@
+#include "Sunyday.h"
+
+void setup() {
+    B_Music();
+    BuzzerPin(9);
+
+}
+
+void loop() {
+  //单曲循环
+  //B_Music();
+
+}
---
+++ Buzzer_Music_Suny/sunyday.h
@@ -0,0 +1,10 @@
+#ifndef SUNYDAY_H_
+#define SUNYDAY_H_
+#include "Arduino.h"
+extern int buzzerPin;
+extern int pin;
+void PlayTone(int prescaler, int duration_ms, int volume);
+void B_Music(void);
+ void BuzzerPin(int pin);
+
+#endif
